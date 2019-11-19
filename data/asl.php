dad3f2552a83f7008dccf246a844a3ab7b9fca8a
Matthew McNeilly <matthew.mcneilly@localhost.localdomain>
2019-11-19 13:38:43 +0000

    Add Ammeon Solutions Theme

---
+++ data/asl.php
@@ -0,0 +1,47 @@
+<!--
+Author: StefanoPicozzi@gmail.com
+Blog: https://StefanoPicozzi.blog
+GitHub: https://github.com/StefanoPicozzi/cotd.git
+Date: 2016
+-->
+
+<?php
+
+// Change item rankings and related attribute data here
+// If adding new entries than also add associated image
+
+$_SESSION['item'] = array();
+
+$_SESSION['item'][0]['name'] = 'ammeon-solutions';
+$_SESSION['item'][0]['rank'] = 1;
+$_SESSION['item'][0]['caption'] = 'Ammeon Solutions';
+$_SESSION['item'][0]['trivia'] = "<p>Ammeon Solutions.</p> <p style='color:silver;font-size:80%;font-style:italic'>Ammeon Solutions will drive you to the future.</p>";
+$_SESSION['item'][0]['theme'] = 'asl';
+$_SESSION['item'][0]['filename'] = 'data/images/asl/image1.png';
+$_SESSION['item'][0]['rating'] = 0;
+
+$_SESSION['item'][0]['name'] = 'asl-and-redhat';
+$_SESSION['item'][0]['rank'] = 1;
+$_SESSION['item'][0]['caption'] = 'Ammeon Solutions & Red Hat';
+$_SESSION['item'][0]['trivia'] = "<p>Ammeon Solutions & Red Hat.</p> <p style='color:silver;font-size:80%;font-style:italic'>Driving Innovation Forward.</p>";
+$_SESSION['item'][0]['theme'] = 'asl';
+$_SESSION['item'][0]['filename'] = 'data/images/asl/image2.jpeg';
+$_SESSION['item'][0]['rating'] = 0;
+
+$_SESSION['item'][0]['name'] = 'asl-consulting';
+$_SESSION['item'][0]['rank'] = 1;
+$_SESSION['item'][0]['caption'] = 'Ammeon Solutions: Consulting';
+$_SESSION['item'][0]['trivia'] = "<p>Ammeon Solutions: Consulting.</p> <p style='color:silver;font-size:80%;font-style:italic'>Fitter Faster.</p>";
+$_SESSION['item'][0]['theme'] = 'asl';
+$_SESSION['item'][0]['filename'] = 'data/images/asl/image3.jpeg';
+$_SESSION['item'][0]['rating'] = 0;
+
+$_SESSION['item'][0]['name'] = 'asl-training';
+$_SESSION['item'][0]['rank'] = 1;
+$_SESSION['item'][0]['caption'] = 'Ammeon Solutions: Agile Lean Training';
+$_SESSION['item'][0]['trivia'] = "<p>Ammeon Solutions: Agile Lean Training.</p> <p style='color:silver;font-size:80%;font-style:italic'>Learn From Those Who Do It Best.</p>";
+$_SESSION['item'][0]['theme'] = 'asl';
+$_SESSION['item'][0]['filename'] = 'data/images/asl/image4.jpeg';
+$_SESSION['item'][0]['rating'] = 0;
+
+?>
