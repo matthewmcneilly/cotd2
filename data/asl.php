8847fbc1d3d09f005a555cdd2426a0aee9d6810b
Matthew McNeilly <matthew.mcneilly@localhost.localdomain>
2019-11-19 15:19:07 +0000

    ASL Theme Add Media and set to default

--- data/asl.php
+++ data/asl.php
@@ -12,36 +12,36 @@ Date: 2016
 
 $_SESSION['item'] = array();
 
-$_SESSION['item'][0]['name'] = 'ammeon-solutions';
+$_SESSION['item'][0]['name'] = 'asl-and-redhat';
 $_SESSION['item'][0]['rank'] = 1;
-$_SESSION['item'][0]['caption'] = 'Ammeon Solutions';
-$_SESSION['item'][0]['trivia'] = "<p>Ammeon Solutions.</p> <p style='color:silver;font-size:80%;font-style:italic'>Ammeon Solutions will drive you to the future.</p>";
+$_SESSION['item'][0]['caption'] = 'Ammeon Solutions & Red Hat';
+$_SESSION['item'][0]['trivia'] = "<p>Ammeon Solutions & Red Hat.</p> <p style='color:silver;font-size:80%;font-style:italic'>Driving Innovation Forward.</p>";
 $_SESSION['item'][0]['theme'] = 'asl';
-$_SESSION['item'][0]['filename'] = 'data/images/asl/image1.png';
-$_SESSION['item'][0]['rating'] = 0;
+$_SESSION['item'][0]['filename'] = 'data/images/asl/image2.jpeg';
+$_SESSION['item'][0]['rating'] = 5;
 
-$_SESSION['item'][1]['name'] = 'asl-and-redhat';
+$_SESSION['item'][1]['name'] = 'asl-consulting';
 $_SESSION['item'][1]['rank'] = 2;
-$_SESSION['item'][1]['caption'] = 'Ammeon Solutions & Red Hat';
-$_SESSION['item'][1]['trivia'] = "<p>Ammeon Solutions & Red Hat.</p> <p style='color:silver;font-size:80%;font-style:italic'>Driving Innovation Forward.</p>";
+$_SESSION['item'][1]['caption'] = 'Ammeon Solutions: Consulting';
+$_SESSION['item'][1]['trivia'] = "<p>Ammeon Solutions: Consulting.</p> <p style='color:silver;font-size:80%;font-style:italic'>Fitter Faster.</p>";
 $_SESSION['item'][1]['theme'] = 'asl';
-$_SESSION['item'][1]['filename'] = 'data/images/asl/image2.jpeg';
-$_SESSION['item'][1]['rating'] = 0;
+$_SESSION['item'][1]['filename'] = 'data/images/asl/image3.jpeg';
+$_SESSION['item'][1]['rating'] = 5;
 
-$_SESSION['item'][2]['name'] = 'asl-consulting';
+$_SESSION['item'][2]['name'] = 'asl-training';
 $_SESSION['item'][2]['rank'] = 3;
-$_SESSION['item'][2]['caption'] = 'Ammeon Solutions: Consulting';
-$_SESSION['item'][2]['trivia'] = "<p>Ammeon Solutions: Consulting.</p> <p style='color:silver;font-size:80%;font-style:italic'>Fitter Faster.</p>";
+$_SESSION['item'][2]['caption'] = 'Ammeon Solutions: Agile Lean Training';
+$_SESSION['item'][2]['trivia'] = "<p>Ammeon Solutions: Agile Lean Training.</p> <p style='color:silver;font-size:80%;font-style:italic'>Learn From Those Who Do It Best.</p>";
 $_SESSION['item'][2]['theme'] = 'asl';
-$_SESSION['item'][2]['filename'] = 'data/images/asl/image3.jpeg';
-$_SESSION['item'][2]['rating'] = 0;
+$_SESSION['item'][2]['filename'] = 'data/images/asl/image4.jpeg';
+$_SESSION['item'][2]['rating'] = 5;
 
-$_SESSION['item'][3]['name'] = 'asl-training';
-$_SESSION['item'][3]['rank'] = 4;
-$_SESSION['item'][3]['caption'] = 'Ammeon Solutions: Agile Lean Training';
-$_SESSION['item'][3]['trivia'] = "<p>Ammeon Solutions: Agile Lean Training.</p> <p style='color:silver;font-size:80%;font-style:italic'>Learn From Those Who Do It Best.</p>";
-$_SESSION['item'][3]['theme'] = 'asl';
-$_SESSION['item'][3]['filename'] = 'data/images/asl/image4.jpeg';
-$_SESSION['item'][3]['rating'] = 0;
+$_SESSION['item'][2]['name'] = 'redhat-openshift';
+$_SESSION['item'][2]['rank'] = 4;
+$_SESSION['item'][2]['caption'] = 'Red Hat: OpenShift';
+$_SESSION['item'][2]['trivia'] = "<p>Red Hat: OpenShift.</p> <p style='color:silver;font-size:80%;font-style:italic'>Container Application Platform.</p>";
+$_SESSION['item'][2]['theme'] = 'asl';
+$_SESSION['item'][2]['filename'] = 'data/images/asl/image5.png';
+$_SESSION['item'][2]['rating'] = 5;
 
 ?>
--- include/selector.php
+++ include/selector.php
@@ -7,7 +7,7 @@ Date: 2016
 
 <?php
 
-$ini_file = '/etc/config/cotd.properties';                                                                                                               
+$ini_file = '/etc/config/cotd.properties';
 
 $dbv2 = getenv('OPENSHIFT_MYSQL_DB_HOST');
 $dbv3 = getenv('DBHOST');
@@ -31,22 +31,22 @@ if ( !empty($dbv2) ) {
     $_SESSION['DB'] = 'true';
 }
 
-// Determine active theme default to cats                                                                                                         
-$_SESSION['selector'] = 'cats';
+// Determine active theme default to cats
+$_SESSION['selector'] = 'asl';
 $selector = getenv('SELECTOR');
 
-if ( !empty($selector) ) {                                                                                                                     
-    $_SESSION['selector'] = $selector;                                                                                                                      
-} elseif ( file_exists($ini_file) ) {                                                                                                                       
-    $ini_array = parse_ini_file($ini_file);                                                                                                                 
-    $_SESSION['selector'] = $ini_array['selector'];                                                                                                         
+if ( !empty($selector) ) {
+    $_SESSION['selector'] = $selector;
+} elseif ( file_exists($ini_file) ) {
+    $ini_array = parse_ini_file($ini_file);
+    $_SESSION['selector'] = $ini_array['selector'];
 } elseif ( $_SESSION['V2'] == 'true' )  {
-    $_SESSION['selector'] = 'pets'; 
-}                                        
+    $_SESSION['selector'] = 'pets';
+}
 
 $service = getenv('SERVICE');
 // Populate theme using local file or remote REST Service or DB
-if ( !empty($service) ) {                                                                                                                     
+if ( !empty($service) ) {
     include('data/rest.php');
 } elseif ( $_SESSION['DB'] == 'true' ) {
     include('data/db.php');
